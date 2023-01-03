8f04e9fea56240d0eaa3ce91837967b958ce2b91
Yogi Wisesa <yogiwisesa@gmail.com>
2023-01-03 12:40:41 +0800

    fix: code element

--- src/components/editor-component/code/index.tsx
+++ src/components/editor-component/code/index.tsx
@@ -2,8 +2,8 @@ import type { RenderElementProps } from "slate-react";
 
 export const Code = ({ attributes, children }: RenderElementProps) => {
   return (
-    <p data-testid="code" className="bg-slate-50 pl-5" {...attributes}>
-      {children}
-    </p>
+    <pre data-testid="code" className="bg-slate-50 pl-5" {...attributes}>
+      <code>{children}</code>
+    </pre>
   );
 };
