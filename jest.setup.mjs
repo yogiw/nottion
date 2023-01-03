5e8884edbac54acca784a793700b15d02d6bc9df
Yogi Wisesa <yogiwisesa@gmail.com>
2023-01-03 11:49:10 +0800

    chore: deps & config

---
+++ jest.setup.mjs
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom/extend-expect";
+import "slate-test-utils/dist/cjs/mocks";
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      addListener: function () {},
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      removeListener: function () {},
+    };
+  };
